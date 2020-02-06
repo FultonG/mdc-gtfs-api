04ad0c08cc6257db6ff1d8a288efb2f03f193a6d
astherath <farceriv@gmail.com>
2020-02-06 15:34:08 -0500

    removed old server files

--- routes/api/routes.js
+++
@@ -1,9 +0,0 @@
-
-const router = require('express').Router();
-const routes = require("../../controllers/routes.js");
-
-router.get("/", (req, res) => {
-  routes.getAllRoutes(req, (status, data = "ok") => res.status(status).send(data));
-});
-
-module.exports = router;
--- routes/api/trips.js
+++
@@ -1,9 +0,0 @@
-
-const router = require('express').Router();
-const trips = require("../../controllers/trips.js");
-
-router.get("/", (req, res) => {
-  trips.getAllTrips(req, (status, data = "ok") => res.status(status).send(data));
-});
-
-module.exports = router;
