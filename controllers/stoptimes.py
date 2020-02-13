3c4364844316f88c9e39f62bc2d37d04e60def2d
Lazaro Hurtado <hurtadolazaro12@gmail.com>
2020-02-12 20:13:11 -0500

    changed stoptimes to return more info

--- controllers/stoptimes.py
+++ controllers/stoptimes.py
@@ -1,12 +1,12 @@
 from bson.json_util import dumps
-from flask import Blueprint, make_response, request
+from flask import Blueprint, make_response, request, jsonify
 from .instances import mongo
 
 stoptimes = Blueprint('stoptimes', __name__)
 
 # set the collection for the blueprint
+collectionstop = mongo.db.stops
 collection = mongo.db.stoptimes
-
 # find all stoptimes by tripID
 @stoptimes.route('/stoptimes/find', methods=['GET'])
 def find_stoptimes_by_id(tripID=None):
@@ -23,6 +23,7 @@ def find_stoptimes_by_id(tripID=None):
     try:
         result = collection.find({'trip_id': trip_id},
                 {'_id': 0, 'arrival_time': 1, 'departure_time': 1, 'stop_id': 1})
+        result = find_stop(result)
     except:
         # return none and 500 if any errors happen
         return make_response({}, 500)
@@ -31,3 +32,18 @@ def find_stoptimes_by_id(tripID=None):
         return make_response(dumps(result), 200)
     else:
         return result
+
+def find_stop(trip_stop_info):
+    # print(trip_stop_info)
+    result = []
+    for stop in trip_stop_info:
+        stop_info = collectionstop.find({'stop_id':stop['stop_id']}, {'_id':0,'stop_name':1,'loc':1})
+        stop_loc_name = stop_info[0]
+        stop_name = stop_loc_name['stop_name']
+        stop_loc = stop_loc_name['loc']
+        stop['stop_name'] = stop_name
+        stop['loc'] = stop_loc
+        result.append(stop)
+        # print(F'stop_name: {stop_name}')
+        # print(F'stop_loc: {stop_loc}')
+    return result
